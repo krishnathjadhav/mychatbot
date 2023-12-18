d060b688d085ef4dee9e2928bebbabf921491712
Krishnath12 <krishnathjadhav@gmail.com>
2023-12-18 14:55:04 +0530

    test chatbot

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,8 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
-
-const packageJson = require("./package.json");
+import packageJson from "./package.json" assert { type: "json" };
+//const packageJson = require("./package.json");
 
 export default [
   {
