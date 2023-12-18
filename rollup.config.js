2b8c255e784d3f555175670cb8f4b52a22720e82
Krishnath12 <krishnathjadhav@gmail.com>
2023-12-18 18:40:41 +0530

    test template

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
-import packageJson from "./package.json" assert { type: "json" };
-//const packageJson = require("./package.json");
+//This plugin prevents packages listed in peerDependencies from being bundled with our component library
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+//import packageJson from "./package.json" assert { type: "json" };
+const packageJson = require("./package.json");
 
 export default [
   {
@@ -22,11 +24,13 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
-    ]
+    ],
+    external: ["react", "react-dom"],
   },
   {
     input: "dist/esm/types/index.d.ts",
