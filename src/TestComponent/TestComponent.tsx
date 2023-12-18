dbd90217afc93e98297ca3080482fc5521ebaed2
Krishnath12 <krishnathjadhav@gmail.com>
2023-12-18 13:00:11 +0530

    test chatbot

--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -4,14 +4,17 @@ import { TestComponentProps } from "./TestComponent.types";
 
 import "./TestComponent.scss";
 
-const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
-  <div
-    data-testid="test-component"
-    className={`test-component test-component-${theme}`}
-  >
-    <h1 className="heading">I'm the test component</h1>
-    <h2>Made with love by Harvey</h2>
-  </div>
-);
+const TestComponent: React.FC<TestComponentProps> = ({ theme }) => {
+  const [cc, setCC] = React.useState(10);
+  return (
+    <div
+      data-testid="test-component"
+      className={`test-component test-component-${theme}`}
+    >
+      <h1 className="heading">I'm the test component</h1>
+      <h2>Made with love by Harvey, see KK:{cc}</h2>
+    </div>
+  );
+};
 
 export default TestComponent;
